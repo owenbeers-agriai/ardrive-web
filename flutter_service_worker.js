84e4fd7758967ff9529e654fe6fbb93f9d827ca2
CDDelta <14818946+CDDelta@users.noreply.github.com>
2020-11-27 03:41:16 +0000

    Deploying to gh-pages from  @ 5932a626a6f6aa98be2594007dab1442ab558ab0 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "sql-wasm.js": "63ac58d843bccce6c3c4b0c1cd6c4422",
 "version.json": "b707f51c1c0147506d74a238ff252173",
 "pst.min.js": "a0563cecdda21dd1f8b4056c7300f842",
-"main.dart.js": "a8486180aceb1a0d2d7dc86c20642606",
+"main.dart.js": "6f09d9485a9744ecb7945fa88e32b5c6",
 "index.html": "3bfcdcbc8d277f21ef53a3b42b4db18a",
 "/": "3bfcdcbc8d277f21ef53a3b42b4db18a",
 "favicon.png": "49623618e34e76925ba5a44f8dc95a89",
