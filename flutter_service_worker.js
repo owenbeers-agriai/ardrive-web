76b2c72adcef73a910cbcec80fb8b6687c3eb9ee
agsuy <37564412+agsuy@users.noreply.github.com>
2021-03-08 01:16:15 +0000

    Deploying to gh-pages from  @ 20ab745c2d359b9c7dc7c1906f0efee7d0c09247 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "sql-wasm.js": "eea55d481cf4aeb2bc2d7c90eec64a25",
-"main.dart.js": "026a01b96c27477122b3715fe07c0b56",
+"main.dart.js": "6981c29858d65ca5a9338bef8b2f8165",
 "icons/Icon-192.png": "885b984ac13230ad7e85436d0bf1adb3",
 "icons/Icon-512.png": "bcad5884196c85e3ac23affa0cc050da",
 "sql-wasm.wasm": "ea7edc8cc0702b48cc93bf41e5b6cc61",
