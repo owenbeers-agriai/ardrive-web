35ae85371fd2816d8f50fe9076b6613b64df938a
agsuy <37564412+agsuy@users.noreply.github.com>
2021-06-08 13:51:20 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@190d451323b388f5417b6de61ca713b7a44609be 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,41 +3,41 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/assets/fonts/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
-"assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
-"assets/assets/fonts/Montserrat-Light.ttf": "409c7f79a42e56c785f50ed37535f0be",
-"assets/assets/fonts/Montserrat-Regular.ttf": "ee6539921d713482b8ccd4d0d23961bb",
-"assets/assets/images/profile/profile_add.png": "bb761b58972221c0bb8a330c1be86f3a",
-"assets/assets/images/profile/profile_permahills_bg.png": "b4d8f83b6bd7750a64cb305bcf1dd1a0",
-"assets/assets/images/profile/profile_new_user_upload.png": "6eb625030efb33a901a7be4531e015da",
-"assets/assets/images/profile/profile_welcome.png": "8ce2b109cbe2aa3155d501be5bc47dba",
-"assets/assets/images/profile/profile_new_user_payment.png": "968d2e6cb2304669cd61d8f614b960a4",
-"assets/assets/images/profile/profile_new_user_delete.png": "6eb625030efb33a901a7be4531e015da",
-"assets/assets/images/profile/profile_unlock.png": "bb761b58972221c0bb8a330c1be86f3a",
-"assets/assets/images/profile/profile_new_user_permanent.png": "19cef59765533319b5ab58d665200762",
-"assets/assets/images/profile/profile_new_user_private.png": "bb761b58972221c0bb8a330c1be86f3a",
-"assets/assets/images/brand/logo-horiz-no-subtitle-light.png": "e97bd96b43d96d3d839c0ed9660bf472",
-"assets/assets/images/brand/logo-vert-no-subtitle.png": "181c34ae51c6053ba9dc18035aac2399",
-"assets/assets/images/brand/logo-horiz-no-subtitle.png": "7f2b427acfa0012da79aa7a8251d148b",
-"assets/assets/images/brand/logo-horiz-no-subtitle-dark.png": "c424b52d9789844c50efd1841f330016",
-"assets/assets/config/dev.json": "3d6c5819f04179660960f51274c830aa",
-"assets/assets/config/prod.json": "3d6c5819f04179660960f51274c830aa",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+  "version.json": "e55e6fa01bb15cb662d1ac9fc029f200",
+"pst.min.js": "4418d27078fa65411dbfc29f909e2ebc",
+"main.dart.js": "171ea9e995efa59c427dd8e04f610e9b",
+"sql-wasm.js": "eea55d481cf4aeb2bc2d7c90eec64a25",
+"index.html": "db700b3d6255b5deb35302c1355158f3",
+"/": "db700b3d6255b5deb35302c1355158f3",
 "assets/packages/flutter_dropzone_web/assets/flutter_dropzone.js": "5ee1f285611168cd6df377fd21151aae",
 "assets/AssetManifest.json": "583e734794cb1c08e4b1c8d582c84c70",
-"assets/NOTICES": "565e2ae7d605cc2a394d750a15892878",
-"sql-wasm.js": "eea55d481cf4aeb2bc2d7c90eec64a25",
+"assets/NOTICES": "d886d4505658bd3953c6d461f7d367f2",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/config/dev.json": "3d6c5819f04179660960f51274c830aa",
+"assets/assets/config/prod.json": "3d6c5819f04179660960f51274c830aa",
+"assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+"assets/assets/fonts/Montserrat-Regular.ttf": "ee6539921d713482b8ccd4d0d23961bb",
+"assets/assets/fonts/Montserrat-Light.ttf": "409c7f79a42e56c785f50ed37535f0be",
+"assets/assets/fonts/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
+"assets/assets/images/profile/profile_add.png": "bb761b58972221c0bb8a330c1be86f3a",
+"assets/assets/images/profile/profile_permahills_bg.png": "b4d8f83b6bd7750a64cb305bcf1dd1a0",
+"assets/assets/images/profile/profile_new_user_delete.png": "6eb625030efb33a901a7be4531e015da",
+"assets/assets/images/profile/profile_new_user_upload.png": "6eb625030efb33a901a7be4531e015da",
+"assets/assets/images/profile/profile_unlock.png": "bb761b58972221c0bb8a330c1be86f3a",
+"assets/assets/images/profile/profile_new_user_private.png": "bb761b58972221c0bb8a330c1be86f3a",
+"assets/assets/images/profile/profile_new_user_payment.png": "968d2e6cb2304669cd61d8f614b960a4",
+"assets/assets/images/profile/profile_new_user_permanent.png": "19cef59765533319b5ab58d665200762",
+"assets/assets/images/profile/profile_welcome.png": "8ce2b109cbe2aa3155d501be5bc47dba",
+"assets/assets/images/brand/logo-vert-no-subtitle.png": "181c34ae51c6053ba9dc18035aac2399",
+"assets/assets/images/brand/logo-horiz-no-subtitle-light.png": "e97bd96b43d96d3d839c0ed9660bf472",
+"assets/assets/images/brand/logo-horiz-no-subtitle-dark.png": "c424b52d9789844c50efd1841f330016",
+"assets/assets/images/brand/logo-horiz-no-subtitle.png": "7f2b427acfa0012da79aa7a8251d148b",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "icons/Icon-512.png": "92dc660f505d120ed1d92db8ec74943d",
 "icons/Icon-192.png": "59fd8ea25636b975179f03f82cc7b19e",
-"pst.min.js": "4418d27078fa65411dbfc29f909e2ebc",
-"sql-wasm.wasm": "ea7edc8cc0702b48cc93bf41e5b6cc61",
-"favicon.png": "9a13599a830ef123989fad7c28d09cc0",
-"version.json": "e55e6fa01bb15cb662d1ac9fc029f200",
 "manifest.json": "a610c0950a2012e01119d0e8a57e3585",
-"main.dart.js": "47433c7a05540a04caca1fbfc9e4e98a",
-"index.html": "db700b3d6255b5deb35302c1355158f3",
-"/": "db700b3d6255b5deb35302c1355158f3"
+"favicon.png": "9a13599a830ef123989fad7c28d09cc0",
+"sql-wasm.wasm": "ea7edc8cc0702b48cc93bf41e5b6cc61"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -55,7 +55,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
