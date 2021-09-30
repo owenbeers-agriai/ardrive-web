4047237084098fa6b57cc0c3b4522335ffdbc529
jdaev <jdhn42@gmail.com>
2021-09-30 23:30:30 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@3d7bb283caf204e8ff1506a50609eb9013adc6e3 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "92dc660f505d120ed1d92db8ec74943d",
 "icons/Icon-192.png": "59fd8ea25636b975179f03f82cc7b19e",
 "js/arconnect.js": "fcf3565603d187e5050b091f013c4d45",
-"main.dart.js": "3097b4a4d62372814dfbcd74413702b2",
+"main.dart.js": "019ef5875aaad981a5e99dc6cea1224b",
 "pst.min.js": "4418d27078fa65411dbfc29f909e2ebc"
 };
 
